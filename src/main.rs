93d7c36d98952ebb98a956d74ba57630e9f18510
Anshul Dalal <anshulusr@gmail.com>
2023-11-19 14:28:42 +0530

    added rendering pipeline
    - Rendered blue tri on red bg

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ struct State {
 	config: wgpu::SurfaceConfiguration,
 	size: winit::dpi::PhysicalSize<u32>,
 	window: Window,
+	pipeline: wgpu::RenderPipeline,
 }
 
 impl State {
@@ -56,6 +57,46 @@ impl State {
 			alpha_mode: surface_capabilities.alpha_modes[0],
 			view_formats: vec![],
 		};
+		let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
+		let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+			label: None,
+			bind_group_layouts: &[],
+			push_constant_ranges: &[],
+		});
+		let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+			label: None,
+			layout: Some(&pipeline_layout),
+			vertex: wgpu::VertexState {
+				module: &shader,
+				entry_point: "vs_main",
+				buffers: &[],
+			},
+			fragment: Some(wgpu::FragmentState {
+				module: &shader,
+				entry_point: "fs_main",
+				targets: &[Some(wgpu::ColorTargetState {
+					format: config.format,
+					blend: Some(wgpu::BlendState::REPLACE),
+					write_mask: wgpu::ColorWrites::ALL,
+				})],
+			}),
+			primitive: wgpu::PrimitiveState {
+				topology: wgpu::PrimitiveTopology::TriangleList,
+				strip_index_format: None,
+				front_face: wgpu::FrontFace::Ccw,
+				cull_mode: Some(wgpu::Face::Back),
+				unclipped_depth: false,
+				polygon_mode: wgpu::PolygonMode::Fill,
+				conservative: false,
+			},
+			depth_stencil: None,
+			multisample: wgpu::MultisampleState {
+				count: 1,
+				mask: !0,
+				alpha_to_coverage_enabled: false,
+			},
+			multiview: None,
+		});
 		Self {
 			surface,
 			device,
@@ -63,6 +104,7 @@ impl State {
 			config,
 			size,
 			window,
+			pipeline,
 		}
 	}
 
@@ -87,7 +129,7 @@ impl State {
 		let mut encoder = self
 			.device
 			.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
-		let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+		let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
 			label: None,
 			color_attachments: &[Some(wgpu::RenderPassColorAttachment {
 				view: &view,
@@ -101,7 +143,9 @@ impl State {
 			timestamp_writes: None,
 			occlusion_query_set: None,
 		});
-		drop(_render_pass);
+		render_pass.set_pipeline(&self.pipeline);
+		render_pass.draw(0..3, 0..1);
+		drop(render_pass);
 		self.queue.submit(std::iter::once(encoder.finish()));
 		output.present();
 		Ok(())
