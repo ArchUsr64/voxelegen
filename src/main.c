589b8ad729aefdbd725fab4036da9c0471fc0a1a
Anshul Dalal <anshulusr@gmail.com>
2025-05-31 15:39:50 +0530

    gl: upgrade to latest opengl

--- src/main.c
+++ src/main.c
@@ -106,8 +106,8 @@ int main(void) {
 	}
 	ret = 0;
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
